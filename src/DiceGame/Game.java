2ab05c54d13f59b4144928d96f48c5bcba217d31
Steven Pedersen <stevengordonpedersen@gmail.com>
2020-09-22 17:41:20 +0200

    Setup the code, projects and classes.

---
+++ src/DiceGame/Game.java
@@ -0,0 +1,14 @@
+package DiceGame;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int dice1, dice2, player1, player2;
+
+
+
+    }
+}
---
+++ src/Test/Test.java
@@ -0,0 +1,8 @@
+package Test;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
