da059f0c6509bcb06ee4b7b6a0ae9fcf1ead05d8
Steven Pedersen <s205456@student.dtu.dk>
2020-09-28 14:15:27 +0200

    Fixed problem in Test

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,8 @@ import DiceGame.Dice;
 
 public class Test {
     public static void main(String[] args) {
-        Dice eye = new Dice();
-        System.out.println(eye.Roll)
+        Dice eye = new Dice(6);
+        System.out.println(eye.Roll());
     }
 
 
