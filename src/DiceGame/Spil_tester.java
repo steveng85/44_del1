c2dd59ac3da524554336f50a4796f6d7e88ea17d
Schnack <simon.schnack99@gmail.com>
2020-09-28 16:55:57 +0200

    Har skiftet "while" om til et "for" loop.

--- src/DiceGame/Spil_tester.java
+++ src/DiceGame/Spil_tester.java
@@ -9,12 +9,12 @@ public class Spil_tester
     {
         Scanner scan = new Scanner( System.in );
         Random rand = new Random();
-        int count = 1;
-        while ( count<=1000 )
+
+        for ( int count=1; count<=1000;count++ )
         {
             System.out.println("Du Slog " + (rand.nextInt(6)+1 + rand.nextInt(6)+1) );
             System.out.println(count + " Gang");
-            count++;
+
 
         }
     }
