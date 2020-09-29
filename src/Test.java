34d45a7a636ad3e9e61a1d97726bf4be749365b7
Steven Pedersen <s205456@student.dtu.dk>
2020-09-29 21:56:14 +0200

    forloop in test

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,16 @@
 import DiceGame.Dice;
+import DiceGame.Game;
 
 public class Test {
     public static void main(String[] args) {
         Dice eye = new Dice();
+        Game eyes = new Game();
 
+        for (int times=0; times<=1000;times++){
+
+
+
+        }
     }
 
 
