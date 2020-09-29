742099a9be9b75e7b10047cda9ac1d192a2050f2
Katrine <KatsCoding@users.noreply.github.com>
2020-09-29 23:05:06 +0200

    skabelon til start på test

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,10 @@ import DiceGame.Dice;
 
 public class Test {
     public static void main(String[] args) {
-        Dice eye = new Dice(6);
-        System.out.println(eye.Roll());
-        System.out.println(eye.textForm());
+        Dice dice1 = new Dice(6);
+        Dice dice2 = new Dice (6);
+
+
     }
 
 
