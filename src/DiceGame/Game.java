03a1d29ab6fcb058b6ddc40ecb4869766661641d
Steven Pedersen <s205456@student.dtu.dk>
2020-09-29 22:18:42 +0200

    fixed rule for eyes then they are both 1

--- src/DiceGame/Game.java
+++ src/DiceGame/Game.java
@@ -21,7 +21,7 @@ public class Game {
     public void checkRules(){
         switch (dice1.getEyeValue()){
             case 1 :
-                currentPlayer.changePoints(0);
+                currentPlayer.setPoints(0);
                 break;
             case 2 :
             case 3 :
