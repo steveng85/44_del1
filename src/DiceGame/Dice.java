e7399b28ff59843df8e6269501544a9b02a7b93c
Schnack <simon.schnack99@gmail.com>
2020-10-01 21:45:23 +0200

    improved version of game

--- src/DiceGame/Dice.java
+++ src/DiceGame/Dice.java
@@ -6,16 +6,36 @@ public class Dice {
     private int eyeValue;
 
     public int Roll(){
+        for (int i = 0; i < (int)(Math.random() * 40) + 1; i++) {
+            System.out.print(textValue((int)(Math.random() * MAX) + 1) + ".");
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
         eyeValue = (int)(Math.random() * MAX) + 1;
+        System.out.print(" --> [" + textForm() + "]\n");
         return eyeValue;
     }
 
     public int getEyeValue() {
         return eyeValue;
     }
-
+    public String textValue(int value) {
+        return switch (value) {
+            case 1 -> "\u2680";
+            case 2 -> "\u2681";
+            case 3 -> "\u2682";
+            case 4 -> "\u2683";
+            case 5 -> "\u2684";
+            default -> "\u2685";
+        };
+    }
     public String textForm(){
-        String result = "You rolled a " + eyeValue;
-        return result;
+        return textValue(eyeValue);
     }
 }
